b89bba0f4a62c8a6b47e4a64e71ea63d5ababe31
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2025-06-21 14:34:09 +0530

    implemented T5 model

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, request, jsonify
 from inference import Translator
+from simpletransformers.t5 import T5Model
 
 app = Flask(__name__)
 translator = Translator(weight_dir='weights')  # Initialize translator
 
-@app.route('/translate', methods=['POST'])
-def translate():
+
+model = T5Model("mt5", "thilina/mt5-sinhalese-english")
+print(model.predict(["Let's translate!"]))
+
+@app.route('/translate/<model_name>', methods=['POST'])
+def translate(model_name=="default"):
     data = request.get_json()
     
     if 'text' not in data or not data['text'].strip():
         return jsonify({'error': 'Missing or empty "text" parameter'}), 400
     
     try:
-        translation = translator.translate(data['text'])
+        if model_name=="default":
+            translation = translator.translate(data['text'])
+        elif model_name=="T5":
+            translation = model.predict(["When will this happen?"])[0]
 
         return jsonify({
             'sinhala': data['text'],
